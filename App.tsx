b5e2941e567a34818d4fb2db491f6a4fba6513ee
yn1323 <yn1323@gmail.com>
2020-06-29 23:56:50 +0900

    Add: Androidに存在しないフォントをインストールする処理を追加

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import Index from './src/pages/Index'
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons'
+// import Spinner from 'react-native-loading-spinner-overlay'
+// import { Container, Content, Grid, Col, Button } from 'native-base'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
+// Androidではnativebaseのフォントがないので、ここで読み込む
+// ComponentWillMountが必要なのでクラスコンポーネントでないといけない？？
+export default class App extends Component {
+  state = { isReady: false }
+
+  async componentDidMount(): Promise<void> {
+    await Font.loadAsync({
+      Roboto: require('./node_modules/native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('./node_modules/native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    })
+    this.setState({ isReady: true })
+  }
+
+  render(): JSX.Element {
+    if (!this.state.isReady) {
+      return <View />
+    } else {
+      return <Index />
+    }
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
