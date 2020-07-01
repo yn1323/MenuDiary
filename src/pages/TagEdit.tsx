f492c7582f5425939c7f68b79bbc2cc85001e389
yn1323 <yn1323@gmail.com>
2020-07-01 22:03:02 +0900

    Add: Pages

---
+++ src/pages/TagEdit.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import { moduleName } from 'native-base'
+// import { moduleName } from 'react-redux';
+
+interface Props {}
+
+type AllProps = Readonly<Props>
+
+export default (): JSX.Element => {
+  return <div></div>
+}
