558356e9bcdfbc7602805935ca3999afb0900919
yn1323 <yn1323@gmail.com>
2020-08-04 23:13:55 +0900

    Fix: 記事を降順で表示

--- src/helpers/common.tsx
+++ src/helpers/common.tsx
@@ -14,12 +14,13 @@ export const now = (): string => moment(new Date()).format('YYYY-MM-DD')
 // postの配列を日付順順に並べ替える
 export const orderPost = (arr: PostState[]): PostState[] => {
   arr.sort(function (a, b) {
-    if (a.regDate < b.regDate) {
+    if (a.regDate > b.regDate) {
       return -1
     } else {
       return 1
     }
   })
+  console.log(arr)
 
   return arr
 }
